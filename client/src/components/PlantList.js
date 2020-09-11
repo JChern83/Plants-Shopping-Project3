92c2f01102a1295282cd76f7017a7855a147187e
JCHern1983 <xnemesis21@gmail.com>
2020-09-11 15:59:06 -0700

    Project3

---
+++ client/src/components/PlantList.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class PlantList extends Component {
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+     plants: []
+  }
+}
+
+componentDidMount () {
+    axios.get(`http://localhost:3333/plants`)
+    .then(res => {
+      console.log(res.data.plantsData)
+      this.setState({plants: res.data.plantsData})
+    })
+    .catch(err =>{
+      console.log(err, 'No data coming in!')
+    })
+
+}
+
+
+
+
+  render() {
+    return (
+      <main className="plant-list">
+        {this.state?.plants?.map((plant) => (
+          <div className="plant-card" key={plant.id}>
+            <img className="plant-image" src={plant.img} alt={plant.name} />
+            <div className="plant-details">
+              <h2 className="plant-name">{plant.name}</h2>
+              <p className="plant-scientific-name">{plant.scientificName}</p>
+              <p>{plant.description}</p>
+              <div className="plant-bottom-row">
+                <p>${plant.price}</p>
+                <p>☀️ {plant.light}</p>
+                <p>💦 {plant.watering}x/month</p>
+              </div>
+              <button
+                className="plant-button"
+                onClick={() => this.props.addToCart(plant)}
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </main>
+    );
+  }
+}
